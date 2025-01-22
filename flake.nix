9f9c8cd9b8f1c2554c4b122438ede41e2c782503
caedesyth <evanev7@gmail.com>
2025-01-22 13:48:07 +0000

    consistentifying flake versions

--- flake.nix
+++ flake.nix
@@ -2,44 +2,53 @@
   description = "RUST FLAKE RUST FLAKE";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
   };
 
-  outputs = { self, nixpkgs }: 
-  let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     system = "x86_64-linux";
-    libPath = with pkgs; lib.makeLibraryPath [
-      libGL
-      libxkbcommon
-      wayland
-    ];
-    pkgs = nixpkgs.legacyPackages.${system};
-  in 
-  with pkgs;
-  {
-    devShells.${system}.default = mkShell rec {
-      buildInputs = [
-        cargo
-        rustc
-        rust-analyzer
-        rustfmt
-        libxkbcommon
-        wayland xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXi
-        alsa-lib
-        shaderc directx-shader-compiler
-
-        bacon
-        clippy
-
+    libPath = with pkgs;
+      lib.makeLibraryPath [
         libGL
-        vulkan-headers vulkan-loader
-        vulkan-tools vulkan-tools-lunarg
-        vulkan-extension-layer
-        vulkan-validation-layers
+        libxkbcommon
+        wayland
       ];
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+    with pkgs; {
+      devShells.${system}.default = mkShell rec {
+        buildInputs = [
+          cargo
+          rustc
+          rust-analyzer
+          rustfmt
+          libxkbcommon
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi
+          alsa-lib
+          shaderc
+          directx-shader-compiler
 
-      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-      LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+          bacon
+          clippy
+
+          libGL
+          vulkan-headers
+          vulkan-loader
+          vulkan-tools
+          vulkan-tools-lunarg
+          vulkan-extension-layer
+          vulkan-validation-layers
+        ];
+
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+      };
     };
-  };
 }
